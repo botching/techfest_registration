9b15a31b2fa964b80b125105fc512815ab3da008
Kalinisan <daniellamarie.kalinisan@benilde.edu.ph>
2025-11-06 08:36:05 +0800

    Implemented participant registration

---
+++ techfest_registration.py
@@ -0,0 +1,10 @@
+print("Welcome to SMIT TechFest!")
+print("Event organized by Daniella Kalinisan of APPDAET BTCS1")
+print()
+
+#Ask the event organizer:
+num = int(input("How many participants will register? "))
+
+if num <= 0:
+    print("Invalid number of participants")
+    quit()
