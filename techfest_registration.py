351790de991d4e87954bd71fa932c2e9f519cd60
Kalinisan <daniellamarie.kalinisan@benilde.edu.ph>
2025-11-06 08:43:38 +0800

    Implemented participant information collection

--- techfest_registration.py
+++ techfest_registration.py
@@ -2,9 +2,24 @@ print("Welcome to SMIT TechFest!")
 print("Event organized by Daniella Kalinisan of APPDAET BTCS1")
 print()
 
+#Task 1: Registration Setup
 #Ask the event organizer:
 num = int(input("How many participants will register? "))
 
 if num <= 0:
     print("Invalid number of participants")
     quit()
+
+#Task 2: Collect Participant Information
+participants = []
+
+for i in range(num):
+    name = input("Enter participant name: ")
+    track = input("Enter chosen track: ")
+
+    record = {"name": name, "track": track}
+    participants.append(record) #add to the list
+
+print("\nRegistered participants:")
+for i in range(len(participants)):
+    print(str(i+1) + ". " + participants[i]['name'] + "-" + participants[i]['track'])
